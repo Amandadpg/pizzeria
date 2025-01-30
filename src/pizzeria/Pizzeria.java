36978718f991e2e55e1447eb1950729d17e7e68f
Alex <alejc2006@gmail.com>
2025-01-30 12:50:32 +0100

    Pizzeria (en proceso)

--- src/pizzeria/Pizzeria.java
+++ src/pizzeria/Pizzeria.java
@@ -2,4 +2,43 @@ package pizzeria;
 
 public class Pizzeria {
 
+	private Cliente cliente;
+	private Pizza pizza;
+	private Pedido pedido;
+	
+	public Pizzeria(Cliente cliente, Pizza pizza, Pedido pedido) {
+		
+		setCliente(cliente);
+		setPedido(pedido);
+		setPizza(pizza);
+	
+	}
+
+	
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Pizza getPizza() {
+		return pizza;
+	}
+
+	public void setPizza(Pizza pizza) {
+		this.pizza = pizza;
+	}
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+	
+	
 }
