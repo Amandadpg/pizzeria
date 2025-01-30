1c63c5960e77921d0d88bea20e0090ad608d5d2c
Alex <alejc2006@gmail.com>
2025-01-30 13:09:12 +0100

    Pizzeria update

--- src/pizzeria/Pizzeria.java
+++ src/pizzeria/Pizzeria.java
@@ -1,44 +1,33 @@
 package pizzeria;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pizzeria {
 
-	private Cliente cliente;
-	private Pizza pizza;
-	private Pedido pedido;
+	private List<Cliente> cliente;
+	private List<Pizza> pizza;
+	private List<Pedido> pedido;
 	
-	public Pizzeria(Cliente cliente, Pizza pizza, Pedido pedido) {
+	public Pizzeria() {
 		
-		setCliente(cliente);
-		setPedido(pedido);
-		setPizza(pizza);
+		this.cliente = new ArrayList<Cliente>();
+		this.pizza = new ArrayList<Pizza>();
+		this.pedido = new ArrayList<Pedido>();
+		
+	}
+
+ public void addPizza(Pizza p){
+	 if(p == null){
+		throw new IllegalArgumentException("La pizza no puede ser nula");
+	 }
+ }
 	
-	}
-
+ public void addCliente(Cliente c) {
+	 if(c == null) {
+		 throw new IllegalArgumentException("El cliente no puede ser nulo");
+	 }
 	
-	
-	public Cliente getCliente() {
-		return cliente;
-	}
+ }
 
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
-
-	public Pizza getPizza() {
-		return pizza;
-	}
-
-	public void setPizza(Pizza pizza) {
-		this.pizza = pizza;
-	}
-
-	public Pedido getPedido() {
-		return pedido;
-	}
-
-	public void setPedido(Pedido pedido) {
-		this.pedido = pedido;
-	}
-	
-	
 }
